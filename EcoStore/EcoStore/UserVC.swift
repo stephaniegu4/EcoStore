2d982b8f4c9a2ad36ac835f9cbbe1217dd8e6ea2
jinwuliu <liujinwu2@gmail.com>
2019-09-14 18:49:51 -0400

    ml testing

--- EcoStore/EcoStore/UserVC.swift
+++ EcoStore/EcoStore/UserVC.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
+import AVKit
 
 class UserVC: CommonViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // starting up the camera
+        let captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice)
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
     }
 }
